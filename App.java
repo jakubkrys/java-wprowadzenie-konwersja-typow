c241a546ee73880682bac7e61e6322d05b8af050
Jakub <jakubkrys@gmail.com>
2020-12-05 23:46:05 +0100

    Wykonanie zadania 1 i 2

---
+++ App.java
@@ -0,0 +1,21 @@
+class App {
+	public static void main(String args[]){
+		// Zadanie 1
+		int employees = 10420;
+		byte newEmployees = 10;
+		
+		employees = newEmployees;
+		System.out.println("-----Zadanie 1-----");
+		System.out.println("Employees: "+employees);
+		System.out.println();
+		
+		// Zadanie 2
+		int zakopane;
+		short citizens = 27266;
+		
+		zakopane = citizens;
+		System.out.println("-----Zadanie 2-----");
+		System.out.println("Zakopane citizens: "+zakopane);
+		System.out.println();
+	}
+}
